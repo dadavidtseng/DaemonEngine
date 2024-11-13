7a7d02395a6f3af50449551a4ea4907d74868d41
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2024-11-13 13:23:21 -0600

    feat: static variable on Rgba8

--- Code/Engine/Core/Rgba8.hpp
+++ Code/Engine/Core/Rgba8.hpp
@@ -3,19 +3,18 @@
 //-----------------------------------------------------------------------------------------------
 struct Rgba8
 {
-	unsigned char r;
-	unsigned char g;
-	unsigned char b;
-	unsigned char a;
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+    unsigned char a;
 
-	// TODO:
-// static const Rgba8 WHITE:
+    static Rgba8 WHITE;
 
-	Rgba8();
-	explicit Rgba8(
-		unsigned char red,
-		unsigned char green,
-		unsigned char blue,
-		unsigned char alpha = 255
-	);
+    Rgba8();
+    explicit Rgba8(
+        unsigned char red,
+        unsigned char green,
+        unsigned char blue,
+        unsigned char alpha = 255
+    );
 };
