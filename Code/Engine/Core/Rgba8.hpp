9db6373cbac98344fbcbbac9732aceeb246cf454
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2024-11-15 20:43:42 -0600

    feat: TODO note for bitmapFont

--- Code/Engine/Core/Rgba8.hpp
+++ Code/Engine/Core/Rgba8.hpp
@@ -8,13 +8,11 @@ struct Rgba8
     unsigned char b;
     unsigned char a;
 
+    // TODO: ADD colors 
     static Rgba8 WHITE;
 
     Rgba8();
-    explicit Rgba8(
-        unsigned char red,
-        unsigned char green,
-        unsigned char blue,
-        unsigned char alpha = 255
-    );
+    explicit Rgba8(unsigned char red, unsigned char green, unsigned char blue, unsigned char alpha = 255);
+
+    // TODO://SetFromTexts(char const* string);
 };
