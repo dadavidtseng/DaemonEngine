113bb470df4bcb7d9e627000258b5f924e1eb69d
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2024-12-02 02:51:13 -0600

    refactor: move g_theBitmapFont to GameCommon

--- Code/Engine/Core/DevConsole.hpp
+++ Code/Engine/Core/DevConsole.hpp
@@ -17,31 +17,31 @@ struct AABB2;
 class DevConsole
 {
 public:
-    DevConsole(DevConsoleConfig const& config);
-    ~DevConsole();
-    void StartUp();
-    void Shutdown();
-    void BeginFrame();
-    void EndFrame();
+    // DevConsole(DevConsoleConfig const& config);
+    // ~DevConsole();
+    // void StartUp();
+    // void Shutdown();
+    // void BeginFrame();
+    // void EndFrame();
+    //
+    // void Execute(String const& consoleCommandText);
+    // void AddLine(Rgba8 const& color, String const& text);
+    // void Render(AABB2 const& bounds, Renderer* rendererOverride = nullptr) const;
 
-    void Execute(String const& consoleCommandText);
-    void AddLine(Rgba8 const& color, String const& text);
-    void Render(AABB2 const& bounds, Renderer* rendererOverride = nullptr) const;
-
-    DevConsoleMode GetMode() const;
-    void           SetMode(DevConsoleMode mode);
-    void           ToggleMode(DevConsoleMode mode);
-
-    static const Rgba8::ERROR;
-    static const Rgba8::WARNING;
-    static const Rgba8::INFO_MAJOR;
-    static const Rgba8::INFO_MINOR;
-
-protected:
-    void Render_OpenFull(AABB2 const& bounds, Renderer& renderer, BitmapFont& font, float fontAspect = 1.f) const;
-
-    DevConsoleConfig            m_config;
-    DevConsoleMode              m_mode = DevConsoleMode::HIDDEN;
-    std::vector<DevConsoleLine> m_lines;     //TODO: support a max limited # of lines (e.g. fixed circular buffer)
-    int                         m_frameNumber = 0;
+//     DevConsoleMode GetMode() const;
+//     void           SetMode(DevConsoleMode mode);
+//     void           ToggleMode(DevConsoleMode mode);
+//
+//     static const Rgba8::ERROR;
+//     static const Rgba8::WARNING;
+//     static const Rgba8::INFO_MAJOR;
+//     static const Rgba8::INFO_MINOR;
+//
+// protected:
+//     void Render_OpenFull(AABB2 const& bounds, Renderer& renderer, BitmapFont& font, float fontAspect = 1.f) const;
+//
+//     DevConsoleConfig            m_config;
+//     DevConsoleMode              m_mode = DevConsoleMode::HIDDEN;
+//     std::vector<DevConsoleLine> m_lines;     //TODO: support a max limited # of lines (e.g. fixed circular buffer)
+//     int                         m_frameNumber = 0;
 };
--- Code/Engine/Core/EventSystem.hpp
+++ Code/Engine/Core/EventSystem.hpp
@@ -9,31 +9,31 @@
 #include "Engine/Core/StringUtils.hpp"
 
 //----------------------------------------------------------------------------------------------------
-typedef std::vector<EventSubscription> SubscriptionList;
+// typedef std::vector<EventSubscription> SubscriptionList;
 
 //----------------------------------------------------------------------------------------------------
 class EventSystem
 {
 public:
-    EventSystem(EventSystemConfig const& config);
-    ~EventSystem();
-    void Startup();
-    void Shutdown();
-    void BeginFrame();
-    void EndFrame();
-
-    void SubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPtr);
-    void UnsubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPrt);
-
-protected:
-    EventSystemConfig                  m_config;
-    std::map<String, SubscriptionList> m_subscriptionsByEventName;
+//     EventSystem(EventSystemConfig const& config);
+//     ~EventSystem();
+//     void Startup();
+//     void Shutdown();
+//     void BeginFrame();
+//     void EndFrame();
+//
+//     void SubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPtr);
+//     void UnsubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPrt);
+//
+// protected:
+//     EventSystemConfig                  m_config;
+//     std::map<String, SubscriptionList> m_subscriptionsByEventName;
 };
 
 //----------------------------------------------------------------------------------------------------
 //  Standalone global-namespace helper functions; these forward to "the" event system, if it exists
 //
-void SubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPtr);
-void UnsubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPrt);
-void FireEvent(String const& eventName, EventArgs& args);
+// void SubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPtr);
+// void UnsubscribeEventCallbackFunction(String const& eventName, EventCallbackFunction functionPrt);
+// void FireEvent(String const& eventName, EventArgs& args);
 void FireEvent(String const& eventName);
