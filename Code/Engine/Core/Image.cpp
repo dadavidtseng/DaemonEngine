de77a4650a926de0edb606638d1fc6936fee3124
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2024-12-01 19:51:30 -0600

    feat: class Image

--- Code/Engine/Core/Image.cpp
+++ Code/Engine/Core/Image.cpp
@@ -1,4 +1,73 @@
+//----------------------------------------------------------------------------------------------------
+// Image.cpp
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
 #define STB_IMAGE_IMPLEMENTATION // Exactly one .CPP (this Image.cpp) should #define this before #including stb_image.h
+#include "Engine/Core/Image.hpp"
+
+#include "Engine/Core/ErrorWarningAssert.hpp"
 #include "ThirdParty/stb/stb_image.h"
 
-#include "Engine/Core/Image.hpp"
+//----------------------------------------------------------------------------------------------------
+Image::Image(char const* imageFilePath)
+    : m_imageFilePath(imageFilePath)
+{
+    int width         = 0;
+    int height        = 0;
+    int numComponents = 0;
+
+    // Load image using stb_image
+    unsigned char* imageData = stbi_load(imageFilePath, &width, &height, &numComponents, 4); // Force 4 components (RGBA)
+    GUARANTEE_OR_DIE(imageData != nullptr, "Failed to load image")
+
+    // Set dimensions
+    m_dimensions = IntVec2(width, height);
+
+    // Copy image data to rgbaTexels
+    size_t const numTexels = static_cast<size_t>(width) * static_cast<size_t>(height);
+    m_rgbaTexels.resize(numTexels);
+
+    for (size_t i = 0; i < numTexels; ++i)
+    {
+        unsigned char const* const pixel = &imageData[i * 4];
+        m_rgbaTexels[i]                  = Rgba8(pixel[0], pixel[1], pixel[2], pixel[3]);
+    }
+
+    // Free the image memory
+    stbi_image_free(imageData);
+}
+
+//----------------------------------------------------------------------------------------------------
+String const& Image::GetImageFilePath() const
+{
+    return m_imageFilePath;
+}
+
+//----------------------------------------------------------------------------------------------------
+IntVec2 Image::GetDimensions() const
+{
+    return m_dimensions;
+}
+
+//----------------------------------------------------------------------------------------------------
+Rgba8 Image::GetTexelColor(IntVec2 const& texelCoords) const
+{
+    GUARANTEE_OR_DIE(texelCoords.x >= 0 && texelCoords.x < m_dimensions.x &&
+                     texelCoords.y >= 0 && texelCoords.y < m_dimensions.y,
+                     "Texel coordinates out of bounds")
+
+    size_t const index = texelCoords.y * m_dimensions.x + texelCoords.x;
+    return m_rgbaTexels[index];
+}
+
+//----------------------------------------------------------------------------------------------------
+void Image::SetTexelColor(IntVec2 const& texelCoords, Rgba8 const& newColor)
+{
+    GUARANTEE_OR_DIE(texelCoords.x >= 0 && texelCoords.x < m_dimensions.x &&
+                     texelCoords.y >= 0 && texelCoords.y < m_dimensions.y,
+                     "Texel coordinates out of bounds")
+
+    size_t const index  = texelCoords.y * m_dimensions.x + texelCoords.x;
+    m_rgbaTexels[index] = newColor;
+}
--- Code/Engine/Core/Image.hpp
+++ Code/Engine/Core/Image.hpp
@@ -1,4 +1,29 @@
+//----------------------------------------------------------------------------------------------------
+// Image.hpp
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
 #pragma once
+#include "Engine/Core/Rgba8.hpp"
+#include "Engine/Core/StringUtils.hpp"
+#include "Engine/Math/IntVec2.hpp"
+
+//----------------------------------------------------------------------------------------------------
 class Image
 {
+public:
+    explicit Image(char const* imageFilePath);
+
+    // Accessors
+    String const& GetImageFilePath() const;
+    IntVec2       GetDimensions() const;
+    Rgba8         GetTexelColor(IntVec2 const& texelCoords) const;
+
+    // Mutators
+    void SetTexelColor(IntVec2 const& texelCoords, Rgba8 const& newColor);
+
+private:
+    String             m_imageFilePath;
+    IntVec2            m_dimensions = IntVec2::ZERO;
+    std::vector<Rgba8> m_rgbaTexels;  // or Rgba8* m_rgbaTexels = nullptr; if you prefer new[] and delete[]
 };
