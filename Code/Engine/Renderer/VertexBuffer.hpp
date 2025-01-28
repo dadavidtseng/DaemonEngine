da0167c1749111d967a1d773972965bd403852d9
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-01-28 00:56:56 -0600

    feat: create VertexBuffer.hpp/.cpp

---
+++ Code/Engine/Renderer/VertexBuffer.hpp
@@ -0,0 +1,34 @@
+//----------------------------------------------------------------------------------------------------
+// Shader.hpp
+//----------------------------------------------------------------------------------------------------
+#pragma once
+#include <d3d11.h>
+
+#include "Engine/Core/StringUtils.hpp"
+
+//----------------------------------------------------------------------------------------------------
+struct ShaderConfig
+{
+    String m_name;
+    String m_vertexEntryPoint = "VertexMain";
+    String m_pixelEntryPoint  = "PixelMain";
+};
+
+//----------------------------------------------------------------------------------------------------
+class Shader
+{
+    friend class Renderer;
+
+public:
+    Shader(ShaderConfig const& config);
+    Shader(Shader& copy) = delete;
+    ~Shader();
+
+    String const& GetName() const;
+
+private:
+    ShaderConfig        m_config;
+    ID3D11VertexShader* m_vertexShader = nullptr;
+    ID3D11PixelShader*  m_pixelShader  = nullptr;
+    ID3D11InputLayout*  m_inputLayout  = nullptr;
+};
