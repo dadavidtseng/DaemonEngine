ee95443c110a29596f4001addb8990bb966bb5cc
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-03-11 15:22:54 -0500

    refactor: project structure

--- Code/Engine/Math/AABB3.hpp
+++ Code/Engine/Math/AABB3.hpp
@@ -11,6 +11,7 @@ struct AABB3
 {
     Vec3 m_mins;
     Vec3 m_maxs;
+
     static AABB3 ZERO_TO_ONE;
     static AABB3 NEG_HALF_TO_HALF;
 
--- Code/Engine/Math/Cylinder3.hpp
+++ Code/Engine/Math/Cylinder3.hpp
@@ -13,11 +13,11 @@ struct Cylinder3
     Vec3  m_endPosition   = Vec3::ZERO;
     float m_radius        = 0.f;
 
-    Cylinder3()                         = default;
-    ~Cylinder3()                        = default;
+    Cylinder3()                          = default;
+    ~Cylinder3()                         = default;
     Cylinder3(Cylinder3 const& copyFrom) = default;
     explicit Cylinder3(Vec3 const& startPosition, Vec3 const& endPosition, float radius);
 
-    bool       IsPointInside(Vec3 const& referencePoint) const;
-    Vec3       GetNearestPoint(Vec3 const& referencePoint) const;
+    bool IsPointInside(Vec3 const& referencePoint) const;
+    Vec3 GetNearestPoint(Vec3 const& referencePoint) const;
 };
