eae5cad2b64f7bf133a7459f6803b6b5064fa903
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-05-15 21:12:25 -0500

    refactor: re-organize RandomNumberGenerator.hpp/.cpp

--- Code/Engine/Math/RandomNumberGenerator.cpp
+++ Code/Engine/Math/RandomNumberGenerator.cpp
@@ -1,31 +1,36 @@
+//----------------------------------------------------------------------------------------------------
+// RandomNumberGenerator.cpp
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
 #include "Engine/Math/RandomNumberGenerator.hpp"
+
 #include <cstdlib>
 
-//-----------------------------------------------------------------------------------------------
-int RandomNumberGenerator::RollRandomIntLessThan(int maxNotInclusive) const
+//----------------------------------------------------------------------------------------------------
+int RandomNumberGenerator::RollRandomIntLessThan(int const maxNotInclusive) const
 {
-	return rand() % maxNotInclusive;
+    return rand() % maxNotInclusive;
 }
 
-int RandomNumberGenerator::RollRandomIntInRange(int minInclusive, int maxInclusive) const
+//----------------------------------------------------------------------------------------------------
+int RandomNumberGenerator::RollRandomIntInRange(int const minInclusive,
+                                                int const maxInclusive) const
 {
-	return rand() % (maxInclusive - minInclusive + 1) + minInclusive;
+    return rand() % (maxInclusive - minInclusive + 1) + minInclusive;
 }
 
+//----------------------------------------------------------------------------------------------------
 float RandomNumberGenerator::RollRandomFloatZeroToOne() const
 {
-	return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    return static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
 }
 
-float RandomNumberGenerator::RollRandomFloatInRange(float minInclusive, float maxInclusive) const
+//----------------------------------------------------------------------------------------------------
+float RandomNumberGenerator::RollRandomFloatInRange(float const minInclusive,
+                                                    float const maxInclusive) const
 {
-	float randomZeroToOne = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+    float const randomZeroToOne = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
 
-	return minInclusive + randomZeroToOne * (maxInclusive - minInclusive);
-}
-
-// TODO: make this
-bool RandomNumberGenerator::RollPercentChance()
-{
-	return false;
+    return minInclusive + randomZeroToOne * (maxInclusive - minInclusive);
 }
--- Code/Engine/Math/RandomNumberGenerator.hpp
+++ Code/Engine/Math/RandomNumberGenerator.hpp
@@ -1,17 +1,20 @@
+//----------------------------------------------------------------------------------------------------
+// RandomNumberGenerator.hpp
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
 #pragma once
 
-//-----------------------------------------------------------------------------------------------
-
+//----------------------------------------------------------------------------------------------------
 class RandomNumberGenerator
 {
 public:
-	int   RollRandomIntLessThan(int maxNotInclusive) const;
-	int   RollRandomIntInRange(int minInclusive, int maxInclusive) const;
-	float RollRandomFloatZeroToOne() const;
-	float RollRandomFloatInRange(float minInclusive, float maxInclusive) const;
-	bool RollPercentChance();
+    int   RollRandomIntLessThan(int maxNotInclusive) const;
+    int   RollRandomIntInRange(int minInclusive, int maxInclusive) const;
+    float RollRandomFloatZeroToOne() const;
+    float RollRandomFloatInRange(float minInclusive, float maxInclusive) const;
 
 private:
-	//unsigned int m_speed = 0;		// We will use these later on...
-	//int m_position = 0;			// ...when we replace rand() with noise
+    unsigned int m_speed    = 0;		// We will use these later on...
+    int          m_position = 0;			// ...when we replace rand() with noise
 };
