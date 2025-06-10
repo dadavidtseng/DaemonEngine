360c4b08087ee3f3f0bac7978d1f93c7ea8301f6
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-06-10 16:29:29 -0500

    fix: Window.hpp/.cpp are now fixed so that all my project could compile

---
+++ Code/Engine/Platform/Window.hpp
@@ -0,0 +1,56 @@
+//----------------------------------------------------------------------------------------------------
+// Window.hpp
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
+#pragma once
+
+#include "Engine/Core/StringUtils.hpp"
+#include "Engine/Math/IntVec2.hpp"
+#include "Engine/Math/Vec2.hpp"
+
+//-Forward-Declaration--------------------------------------------------------------------------------
+class InputSystem;
+
+//----------------------------------------------------------------------------------------------------
+struct sWindowConfig
+{
+    InputSystem*   m_inputSystem = nullptr;
+    float          m_aspectRatio = 16.f / 9.f;
+    String         m_consoleTitle[11];
+    String         m_windowTitle  = "DEFAULT";
+    wchar_t const* m_iconFilePath = nullptr;
+};
+
+//----------------------------------------------------------------------------------------------------
+class Window
+{
+    friend class Renderer;
+
+public:
+    explicit Window(sWindowConfig const& config);
+    ~Window() = default;
+    void Startup();
+    void Shutdown();
+
+    void BeginFrame();
+    void EndFrame();
+
+    sWindowConfig const& GetConfig() const;
+    void*                GetDisplayContext() const;
+    void*                GetWindowHandle() const;
+    Vec2                 GetNormalizedMouseUV() const;
+    IntVec2              GetClientDimensions() const;
+
+    static Window* s_mainWindow; // fancy way of advertising global variables (advertisement)
+
+private:
+    void CreateOSWindow();
+    void CreateConsole();
+    void RunMessagePump();
+
+    sWindowConfig m_config;
+    void*         m_windowHandle     = nullptr;          // Actually a Windows HWND (Handle of Window) on the Windows platform
+    void*         m_displayContext   = nullptr;          // Actually a Windows HDC (Handle to Device Context) on the Windows platform
+    IntVec2       m_clientDimensions = IntVec2::ZERO;
+};
