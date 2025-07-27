79c30587c3fe7cefd152a2d1a80507ad48757d67
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-07-27 03:24:20 -0500

    update: conf.py

--- Docs/source/conf.py
+++ Docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Engine'
-copyright = '2025, Your Name'
-author = 'Your Name'
+copyright = '2025, Yu-Wei Tseng'
+author = 'Yu-Wei Tseng'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
@@ -12,20 +12,26 @@ release = '1.0.0'
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'sphinx.ext.intersphinx',
     'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+# The master toctree document.
+master_doc = 'index'
+
 # -- Options for HTML output ------------------------------------------------
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
-# -- Options for intersphinx extension ---------------------------------------
+# -- Theme options -----------------------------------------------------------
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False
 }
